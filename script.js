92bc07f56e3bece14b1b21488a062b63b3e42df7
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-06-27 13:00:02 +0200

    added email validation

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
+// Modules
+import validateEmail from "/validateEmail.js";
+
 // Get elements
 const signUpCard = document.getElementById("sign-up-card");
 const modal = document.getElementById("success-message-modal");
 const openButton = document.getElementById("subscribe-button");
 const closeButton = document.getElementById("dismiss-button");
+const input = document.getElementById("email");
+const errorMessage = document.getElementById("error-message");
+const target = document.getElementById("user-email");
 
-// Hide sign up card and show modal 
-openButton.onclick = function () {
-    signUpCard.style.display = "none";
-    modal.style.display = "flex";
+// Handle subscribe button click
+openButton.onclick = function (e) {
+    // Prevent input being cleared
+    e.preventDefault();
+    // Get user input
+    const email = input.value;
+    // Validate user input
+    if (validateEmail(email)) {
+        // Hide error message
+        errorMessage.style.display = "none";
+        input.classList.remove("error");
+        // Hide sign up card
+        signUpCard.style.display = "none";
+        // Insert user input in HTML
+        target.textContent = email;
+        // Show modal
+        modal.style.display = "flex";
+    } else {
+        // Show error message
+        errorMessage.style.display = "flex";
+        input.classList.add("error");
+    }
 }
 
-// Hide modal and show sign up card
+// Handle dismiss button click
 closeButton.onclick = function () {
+    // Hide modal
     modal.style.display = "none";
+    // Show sign up card
     signUpCard.style.display = "flex";
+    // Clear input
+    input.value = '';
 }
